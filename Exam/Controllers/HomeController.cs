46b0809243cd82ddb70c8e23a1bf9391fe98c366
Александровна Татьяна <tanyatt@mail.ru>
2021-08-18 17:06:49 +0300

    Контрольное задание

---
+++ Exam/Controllers/HomeController.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+using Exam.Models;
+
+namespace Exam.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            GiveStudents();
+            return View();
+        }
+
+       /* public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }*/
+        private StudentContext db = new StudentContext();
+        private void GiveStudents()
+        {
+            var allStudents = db.Students.ToList<Student>();
+            ViewBag.Students = allStudents;
+        }
+        [HttpGet]
+        public ActionResult CreateBid()
+        {
+            GiveStudents();
+            var allBids = db.Students.ToList<Student>();
+            ViewBag.Students = allBids;
+            return View();
+        }
+
+        [HttpPost]
+        public string CreateBid(Student newBid)
+        {            
+            // Добавляем новую заявку в БД
+            db.Students.Add(newBid);
+            // Сохраняем в БД все изменения
+            db.SaveChanges();
+            return "Студент, <b>" + newBid.FirstName + newBid.LastName + "</b> добавлен";
+        }
+
+        public ActionResult TotalSerch_max()
+        {             
+            var students = "";
+            foreach (var item in db.Students.ToList<Student>().OrderByDescending(s => s.Total).Take(5))
+            {
+                students += item.FirstName + " " + item.LastName + " " + item.Total + Environment.NewLine;
+            }
+            var byteArray = Encoding.UTF8.GetBytes(students);
+            var stream = new MemoryStream(byteArray);
+            return File(stream, "text/plain", "Список студентов_max.txt");
+        }
+        public ActionResult TotalSerch_min()
+        {
+           
+            var students = "";
+            foreach (var item in db.Students.ToList<Student>().OrderBy(s => s.Total).Take(5))
+            {
+                students += item.FirstName + " " + item.LastName + " " + item.Total + Environment.NewLine;
+            }
+            var byteArray = Encoding.UTF8.GetBytes(students);
+            var stream = new MemoryStream(byteArray);
+            return File(stream, "text/plain", "Список студентов_min.txt");
+        }
+        
+
+
+
+    }
+}
---
+++ Exam/Models/Student.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel;
+
+namespace Exam.Models
+{
+    public class Student
+    {
+        public virtual int StudentId { get; set; }
+
+        [DisplayName("Имя")]
+        public virtual string FirstName { get; set; }
+
+        [DisplayName("Фамилия")]
+        public virtual string LastName { get; set; }
+
+        [DisplayName("Факультет")]
+        public virtual string Faculty { get; set; }
+
+        [DisplayName("Группа")]
+        public virtual string Group { get; set; }
+
+        [DisplayName("Теплофизика")]
+        public virtual int Value_ThermalPhysics { get; set; }
+
+        [DisplayName("Механика жидкости и  газов")]
+        public virtual int Value_Fluid { get; set; }
+
+        [DisplayName("Лопаточные машины")]
+        public virtual int Value_Turbine { get; set; }
+
+        [DisplayName("Криогенная техника")]
+        public virtual int Value_Cryogenic { get; set; }
+
+        [DisplayName("Сумма баллов")]
+        public virtual int Total { get { return Value_ThermalPhysics + Value_Fluid + Value_Turbine + Value_Cryogenic ; } }
+    }
+}
